39ce486da896c8fb68c8206e82808b7a314f3d21
Roman Hrydin <romario198901@gmail.com>
2025-11-18 21:41:04 +0200

    complete task2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,43 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+iziToast.show({
+  title: 'Hello',
+  titleColor: '#FFF',
+  message: 'Welcome',
+  messageColor: '#FFF',
+  backgroundColor: '#09F',
+  position: 'bottomRight',
+});
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const delay = form.elements.delay.value;
+  const status = form.elements.state.value;
+  new Promise((res, rej) => {
+    setTimeout(() => {
+      if (status === 'fulfilled') {
+        res(
+          iziToast.success({
+            title: '✅',
+            message: `Fulfilled promise in ${delay}ms`,
+            titleColor: '#fff',
+            messageColor: '#fff',
+            backgroundColor: '#59a10d',
+          })
+        );
+      } else {
+        rej(
+          iziToast.error({
+            title: '❌',
+            titleColor: '#fff',
+            message: `Rejected promise in ${delay}ms`,
+            messageColor: '#fff',
+            backgroundColor: '#ef4040',
+          })
+        );
+      }
+    }, delay);
+  });
+  form.reset();
+});
